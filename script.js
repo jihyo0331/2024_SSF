027be3362fd3dbdfd06b0b25a2e0e9f033d816b6
Jeong Jihyo <jihyo0331@gmail.com>
2024-08-06 21:26:34 +0900

    1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //script.js
+const bground = document.querySelector(".Bground")
+
+
+
+
+for(let i = 0; i < 200; i++){
+    bground.insertAdjacentHTML("afterbegin",'<div class="snow"></div>')
+}
+
+
+
